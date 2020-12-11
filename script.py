50a61cd3d3f8f8c43ead46160b882c663b46c2e5
oliviergimenez <olivier.gimenez@cefe.cnrs.fr>
2020-12-11 10:03:48 +0100

    first commit :tada:

---
+++ script.py
@@ -0,0 +1,42 @@
+"""
+Training Dec 11
+"""
+
+# Hello world
+print("hello world!")
+test = "hello world"
+print(test)
+
+# tuple
+bla = (4, 5, "booh", True)
+print(bla)
+print(bla[0])
+print(bla[-1]) # dernier element
+
+print(len(bla))
+
+# lists
+
+bla = [4, 5, "booh", True]
+print(bla)
+bla.append()
+
+# dictionnaires
+
+# sets
+setA = {4, 5, "hi", 42}
+print(setA)
+setA.add(44)
+print(setA)
+setB = {4, 5, 122, 826}
+print(setA | setB)
+print(setA - setB)
+
+# conditions
+
+if True:
+  print("ok")
+elif False:
+  print("ttttt")
+else:
+  print("impossible")
